5e8e70a38378120ebfbdfe6de7f21841408085e0
Anirudh Vadakedath <60284251+anirudhv14@users.noreply.github.com>
2021-05-03 14:12:45 +0530

    Update train.py

--- train.py
+++ train.py
@@ -140,8 +140,7 @@ for i in range(4,X.shape[1]):
     X_modified.extend(X[i])
 
 p = players.tolist()
-p.extend(["Jhye Richardson","Chetan Sakariya","Fabian Allen",'Riley Meredith',"D Malan","A Singh,"_"])
-# A Singh is Arshdeep Singh
+p.extend(["Jhye Richardson","Chetan Sakariya","Fabian Allen",'Riley Meredith',"D Malan","_"])
 players = np.array(p)
 le_players = preprocessing.LabelEncoder().fit(players)
 
