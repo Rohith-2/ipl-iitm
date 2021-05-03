877463e33e56baf09200dbd371aac37419d699fb
rohith <rrohith2001@gmail.com>
2021-05-03 12:41:01 +0530

    bug
    Co-Authored-By: Anirudh Vadakedath <60284251+anirudhv14@users.noreply.github.com>

--- train.py
+++ train.py
@@ -140,7 +140,7 @@ for i in range(4,X.shape[1]):
     X_modified.extend(X[i])
 
 p = players.tolist()
-p.extend(["Jhye Richardson","Chetan Sakariya","Fabian Allen",'Riley Meredith',"_"])
+p.extend(["Jhye Richardson","Chetan Sakariya","Fabian Allen",'Riley Meredith',"D Malan","_"])
 players = np.array(p)
 le_players = preprocessing.LabelEncoder().fit(players)
 
